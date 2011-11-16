781e753414765a536b9d016f259f560bd0894c25
Jason Capehart <jasontc2@yahoo.com>
2011-11-16 12:15:12 -0500

    add a test R file

---
+++ RfileTest.R
@@ -0,0 +1,10 @@
+# Markov Language R File
+
+# This is a test file just to make sure there are no
+#   issues collaborating i.e. that we can all check out
+#   a file, edit, add it, commit the changes, and push
+#   the changes
+
+a <- runif(n = 10)
+b <- runif(n = 10)
+plot(a, b)

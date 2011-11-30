869bf3ff56fd7bb0f1d352327f643b1f834304fa
Jason Capehart <jasontc2@yahoo.com>
2011-11-30 13:31:21 -0500

    Updates to part.1() and perplexity()

--- text_vect.R
+++ text_vect.R
@@ -3,14 +3,11 @@
 # 36-350: Final Project - part 1
 
 # Function takes a text document 
-part.1<-function(text="pg526.txt"){
-
-	library(Rstem)
-	library(plyr)
+part.1<-function(text="pg526.txt", stem=FALSE){
 
 	text<-readLines(text)
 
-	text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end points of the license document. 
+	text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end points of the HOD document. 
 
 
 	pat<-c("[[:alpha:]]*||\\.")
@@ -26,11 +23,13 @@ part.1<-function(text="pg526.txt"){
 
 	words.vector<-unlist(words) # Just converting list to vector
 
-	# This is the stemming part of the code - not sure if this is what was wanted as output.	
-	final.words<-wordStem(words.vector, language="english") 
-
-
-	return(final.words)
+	if(stem==TRUE){
+		library(Rstem)
+		final.words<-wordStem(words.vector, language="english") 
+		return(final.words)
+	}
+	else
+	return(words.vector)
 }
 
 

51b2250eebf907120fe3d4461516baa759a59e01
Jason Capehart <jasontc2@yahoo.com>
2011-12-11 19:28:37 -0500

    Update to text parsers

--- MarkovLanguage.R
+++ MarkovLanguage.R
@@ -7,6 +7,7 @@
 # Function List
 # 1) Text Processing
 #   a) part.1()        -  a text processing function
+#   b) htmlToText()    - an HTML parser for pseudo-Cosma
 # 2) Markov Matrix
 #   a) mm.generator()  - Generates a markov matrix  
 #   b) actual.states() - Called by mm.generator(). Produces ngram frequencies and lookup tables
@@ -27,6 +28,7 @@
 #   d) out.of.vocab()    - The rate at which test words do not appear in the training corpus
 #   e) avg.likelihood()  - Average log likelihood of transistion states according to markov matrix
 #   f) avg.prob()        - Average probability of transition states as calculated by markov matrix
+#   g) out.of.ngram()    - Calculates the out of n-gram Rate
 # 5) Smoothing
 #   a) laplace.smoother()  - Add 1 smoothing
 #   b) gte.smoother()      - Good Turing Estimate Smoother
@@ -45,22 +47,33 @@ library(foreach)
 #_________________________
 # (1) Text Processing
 #_________________________
+#_________________________
+# (1) Text Processing
+#_________________________
 #--------------
 # (1.a) part.1() - Text Processing Function
 #---------------
-# Alexander Murray-Watters
-# amurrayw
-# 36-350: Final Project - part 1
+# Function takes a text document 
+# Commands used to construct sample html file for pseudo cosma.
+# (Done from bash terminal)
+# curl -O http://cscs.umich.edu/~crshalizi/weblog/[600-841].html  
+# cat [7-8][0-3]*.html > blog.html
+# Number of words in this file (after html stripped out) = 34532
 
 # Function takes a text document 
-part.1<-function(text="pg526.txt", stem=FALSE){
+part.1<-function(text="pg526.txt", stem=FALSE, cosma=FALSE){
 
   text<-readLines(text)
+  if(cosma==TRUE){
+		text<-paste("blog.html", collapse='\n')
+		text<-htmlToText(text)		
+		text<-gsub("Three-Toed Sloth", " ", x=text)
+	}
+	else{
+		text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end parts of the HOD document. 
+	}
 
-	text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end points of the HOD document. 
-
-
-	pat<-c("[[:alpha:]]*||\\.")
+	pat<-c("[[:alpha:]]*||\\.||\\!||\\?")
 
 	m <- gregexpr(pat, text, ignore.case = TRUE)
 	x <- regmatches(text, m)
@@ -68,7 +81,6 @@ part.1<-function(text="pg526.txt", stem=FALSE){
 
 	m <- regexec(pat, x, ignore.case = TRUE)
 	words <- regmatches(x, m)
-
 	words[words==""]<-NULL # Removes empty strings
 
 	words.vector<-unlist(words) # Just converting list to vector
@@ -78,8 +90,73 @@ part.1<-function(text="pg526.txt", stem=FALSE){
 		final.words<-wordStem(words.vector, language="english") 
 		return(final.words)
 	}
-	else
 	return(words.vector)
+}
+
+# Used for pseudo-cosma. Not written by group. Since was not required for assignment, code used from external source, provided it was cited, should be ok.
+
+# Author: Tony Breyal
+# Date: 2011-11-18
+# Modified: 2011-11-18
+# Description: Extracts all text from a webpage (aims to extract only the text you would see in a web browser)
+# Packages Used: RCurl, XML   
+# Blog Reference: Not published
+
+# Copyright (c) 2011, under the Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0) License
+# For more information see: https://creativecommons.org/licenses/by-nc/3.0/
+# All rights reserved.
+
+htmlToText <- function(input, ...) {
+  ###---PACKAGES ---###
+  require(RCurl)
+  require(XML)
+  
+  
+  ###--- LOCAL FUNCTIONS ---###
+  # Determine how to grab html for a single input element
+  evaluate_input <- function(input) {    
+    # if input is a .html file
+    if(file.exists(input)) {
+      char.vec <- readLines(input, warn = FALSE)
+      return(paste(char.vec, collapse = ""))
+    }
+    
+    # if input is html text
+    if(grepl("</html>", input, fixed = TRUE)) return(input)
+    
+    # if input is a URL, probably should use a regex here instead?
+    if(!grepl(" ", input)) {
+      # downolad SSL certificate in case of https problem
+      if(!file.exists("cacert.perm")) download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.perm")
+      return(getURL(input, followlocation = TRUE, cainfo = "cacert.perm"))
+    }
+    
+    # return NULL if none of the conditions above apply
+    return(NULL)
+  }
+  
+  # convert HTML to plain text
+  convert_html_to_text <- function(html) {
+    doc <- htmlParse(html, asText = TRUE)
+    text <- xpathSApply(doc, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+    return(text)
+  }
+  
+  # format text vector into one character string
+  collapse_text <- function(txt) {
+    return(paste(txt, collapse = " "))
+  }
+  
+  ###--- MAIN ---###
+  # STEP 1: Evaluate input
+  html.list <- lapply(input, evaluate_input)
+  
+  # STEP 2: Extract text from HTML
+  text.list <- lapply(html.list, convert_html_to_text)
+  
+  # STEP 3: Return text
+  text.vector <- sapply(text.list, collapse_text)
+  return(text.vector)
 }
 
 
--- text_vect.R
+++ text_vect.R
@@ -2,15 +2,27 @@
 # amurrayw
 # 36-350: Final Project - part 1
 
+# Commands used to construct sample html file for pseudo cosma.
+# (Done from bash terminal)
+# curl -O http://cscs.umich.edu/~crshalizi/weblog/[600-841].html  
+# cat [7-8][0-3]*.html > blog.html
+# Number of words in this file (after html stripped out) = 34532
+
+
 # Function takes a text document 
-part.1<-function(text="pg526.txt", stem=FALSE){
+part.1<-function(text="pg526.txt", stem=FALSE, cosma=FALSE){
 
-	text<-readLines(text)
+  text<-readLines(text)
+	if(cosma==TRUE){
+		text<-paste("blog.html", collapse='\n')
+		text<-htmlToText(text)		
+		text<-gsub("Three-Toed Sloth", " ", x=text)
+	}
+	else{
+		text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end parts of the HOD document. 
+	}
 
-	text<-paste(text[40:((length(text)-361))], collapse='\n') # removing parts of file which pertain to license. 40 and -361 correspond to the start/end points of the HOD document. 
-
-
-	pat<-c("[[:alpha:]]*||\\.")
+	pat<-c("[[:alpha:]]*||\\.||\\!||\\?")
 
 	m <- gregexpr(pat, text, ignore.case = TRUE)
 	x <- regmatches(text, m)
@@ -18,7 +30,6 @@ part.1<-function(text="pg526.txt", stem=FALSE){
 
 	m <- regexec(pat, x, ignore.case = TRUE)
 	words <- regmatches(x, m)
-
 	words[words==""]<-NULL # Removes empty strings
 
 	words.vector<-unlist(words) # Just converting list to vector
@@ -28,9 +39,75 @@ part.1<-function(text="pg526.txt", stem=FALSE){
 		final.words<-wordStem(words.vector, language="english") 
 		return(final.words)
 	}
-	else
 	return(words.vector)
 }
 
 
 
+
+
+# Used for pseudo-cosma. Not written by group. Since was not required for assignment, code used from external source, provided it was cited, should be ok.
+
+# Author: Tony Breyal
+# Date: 2011-11-18
+# Modified: 2011-11-18
+# Description: Extracts all text from a webpage (aims to extract only the text you would see in a web browser)
+# Packages Used: RCurl, XML   
+# Blog Reference: Not published
+
+# Copyright (c) 2011, under the Creative Commons Attribution-NonCommercial 3.0 Unported (CC BY-NC 3.0) License
+# For more information see: https://creativecommons.org/licenses/by-nc/3.0/
+# All rights reserved.
+
+htmlToText <- function(input, ...) {
+  ###---PACKAGES ---###
+  require(RCurl)
+  require(XML)
+  
+  
+  ###--- LOCAL FUNCTIONS ---###
+  # Determine how to grab html for a single input element
+  evaluate_input <- function(input) {    
+    # if input is a .html file
+    if(file.exists(input)) {
+      char.vec <- readLines(input, warn = FALSE)
+      return(paste(char.vec, collapse = ""))
+    }
+    
+    # if input is html text
+    if(grepl("</html>", input, fixed = TRUE)) return(input)
+    
+    # if input is a URL, probably should use a regex here instead?
+    if(!grepl(" ", input)) {
+      # downolad SSL certificate in case of https problem
+      if(!file.exists("cacert.perm")) download.file(url="http://curl.haxx.se/ca/cacert.pem", destfile="cacert.perm")
+      return(getURL(input, followlocation = TRUE, cainfo = "cacert.perm"))
+    }
+    
+    # return NULL if none of the conditions above apply
+    return(NULL)
+  }
+  
+  # convert HTML to plain text
+  convert_html_to_text <- function(html) {
+    doc <- htmlParse(html, asText = TRUE)
+    text <- xpathSApply(doc, "//text()[not(ancestor::script)][not(ancestor::style)][not(ancestor::noscript)][not(ancestor::form)]", xmlValue)
+    return(text)
+  }
+  
+  # format text vector into one character string
+  collapse_text <- function(txt) {
+    return(paste(txt, collapse = " "))
+  }
+  
+  ###--- MAIN ---###
+  # STEP 1: Evaluate input
+  html.list <- lapply(input, evaluate_input)
+  
+  # STEP 2: Extract text from HTML
+  text.list <- lapply(html.list, convert_html_to_text)
+  
+  # STEP 3: Return text
+  text.vector <- sapply(text.list, collapse_text)
+  return(text.vector)
+}
